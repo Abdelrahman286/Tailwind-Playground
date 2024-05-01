58622bb06361d050451ec60204692eaed5885ca8
Abdelrahman <abdelrahman.design7@gmail.com>
2024-05-01 03:45:23 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-red-500 shadow rounded p-8 m-20  ">
+      <h1 className=" text-3xl font-body">Dsadsa</h1>
+      <h1 className=" text-3xl font-Nunito">Dsadsa</h1>
+      <h1 className=" text-3xl ">Dsadsa</h1>
+    </div>
+  );
+}
+
+export default App;
