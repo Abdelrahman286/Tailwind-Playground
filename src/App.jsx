7ce67592c7b3310c721e8179ef1ed78312f5bcd1
Abdelrahman <abdelrahman.design7@gmail.com>
2024-05-01 03:47:51 +0300

    Adding google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="bg-red-500 shadow rounded p-8 m-20  ">
       <h1 className=" text-3xl font-body">Dsadsa</h1>
       <h1 className=" text-3xl font-Nunito">Dsadsa</h1>
-      <h1 className=" text-3xl ">Dsadsa</h1>
+      <h1 className=" text-3xl ">AA</h1>
     </div>
   );
 }
